b636b164d4f9c765869c20b6606da6dbf6dee88f
Nikita Sachik <nikitasachik@gmail.com>
2024-03-09 06:01:41 +0200

    Find min, max num, median and GreatestSequenceOfNumbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,7 +29,54 @@ public class Main {
         }
         int amountOfNumbers = numbersFromFileList.size();
         double arithmeticAverage = (double)sumOfNumbers / amountOfNumbers;
+        List<Integer> greatestSequenceOfNumbers = findGreatestSequenceOfNumbers(numbersFromFileList);
+        List<Integer> smallestSequenceOfNumbers = findSmallestDecreasingSequence(numbersFromFileList);
         numbersFromFileList.sort(null);
-        System.out.println(sumOfNumbers + " " +amountOfNumbers + " " + arithmeticAverage);
+        int maxNum = numbersFromFileList.get(0);
+        int minNum = numbersFromFileList.get(amountOfNumbers - 1);
+        double median = findMedian(numbersFromFileList, amountOfNumbers);
+        System.out.println("Максимальне значення: " + maxNum);
+        System.out.println("Мінимальне значення: " + minNum);
+        System.out.println("Середнє арифметичне: " + arithmeticAverage);
+        System.out.println("Медіана: " + median);
+        System.out.println("Найбільша послідовність чисел (які ідуть один за одним), яка збільшується: "
+                + greatestSequenceOfNumbers);
+        System.out.println("Найбільша послідовність чисел (які ідуть один за одним), яка зменшується: "
+                + smallestSequenceOfNumbers);
+
+    }
+
+    public static List<Integer> findGreatestSequenceOfNumbers(List<Integer> numbersFromFileList) {
+        List<Integer> greatestSequenceOfNumbers = new ArrayList<>();
+        List<Integer> greatestSequenceOfNumbersTmp = new ArrayList<>();
+        for (int i = 0; i < numbersFromFileList.size() - 1; i++) {
+            greatestSequenceOfNumbersTmp.add(numbersFromFileList.get(i));
+            if (numbersFromFileList.get(i) >= numbersFromFileList.get(i + 1)) {
+                if (greatestSequenceOfNumbers.size() < greatestSequenceOfNumbersTmp.size()) {
+                    greatestSequenceOfNumbers = new ArrayList<>(greatestSequenceOfNumbersTmp);
+                }
+                greatestSequenceOfNumbersTmp.clear();
+            }
+        }
+        if (greatestSequenceOfNumbers.size() < greatestSequenceOfNumbersTmp.size()) {
+            greatestSequenceOfNumbers = new ArrayList<>(greatestSequenceOfNumbersTmp);
+        }
+        return greatestSequenceOfNumbers;
+    }
+
+    public static List<Integer> findSmallestDecreasingSequence(List<Integer> numbersFromFileList) {
+
+        List<Integer> smallestSequenceOfNumbers = new ArrayList<>();
+        List<Integer> smallestSequenceOfNumbersTmp = new ArrayList<>();
+        return smallestSequenceOfNumbers;
+    }
+
+
+    public static double findMedian(List<Integer> numbersFromFileList, int amountOfNumbers) {
+        if (amountOfNumbers % 2 == 0) {
+            return (double)(((numbersFromFileList.get(amountOfNumbers / 2))) + ((numbersFromFileList.get((amountOfNumbers / 2) - 1)))) / 2;
+        } else {
+            return (double)(numbersFromFileList.get((amountOfNumbers) / 2)) ;
+        }
     }
 }
