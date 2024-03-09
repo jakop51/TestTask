39f69bdef447b97213d28fba2b921d2ba2a4bcf9
Nikita Sachik <nikitasachik@gmail.com>
2024-03-09 03:32:50 +0200

    Find arithmetic average

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         File fileWithNumbers = new File("10m.txt");
         List<Integer> numbersFromFileList = new ArrayList<>();
+        long sumOfNumbers = 0;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileWithNumbers))) {
             String currentLine = bufferedReader.readLine();
             int currentNumber;
@@ -20,10 +21,15 @@ public class Main {
                 } catch (NumberFormatException e) {
                     throw new RuntimeException("Can`t parse String to Integer", e);
                 }
+                sumOfNumbers += currentNumber;
                 currentLine = bufferedReader.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Some problems with file", e);
         }
+        int amountOfNumbers = numbersFromFileList.size();
+        double arithmeticAverage = (double)sumOfNumbers / amountOfNumbers;
+        numbersFromFileList.sort(null);
+        System.out.println(sumOfNumbers + " " +amountOfNumbers + " " + arithmeticAverage);
     }
 }
